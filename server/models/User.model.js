e72f0333fcbe0f1441de1ceabfce3baf80f8fe69
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-19 18:43:42 +0530

    user model is added

---
+++ server/models/User.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Export model
+const UserModel = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default UserModel;
