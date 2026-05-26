87c369421a69f4e69885a64655b56408c9dfcb7b
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-26 18:08:06 +0530

    changes in user model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
+    image: {
+      type: String,
+      default: "",
+    },
     name: {
       type: String,
       trim: true,
@@ -12,6 +17,7 @@ const userSchema = new mongoose.Schema(
       unique: true,
       required: true,
       trim: true,
+      lowercase: true,
       match: [
         /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
         "Please enter a valid email",
@@ -21,12 +27,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       trim: true,
+      minlength: [6, "Password must be at least 6 characters"],
     },
   },
   {
     timestamps: true,
   }
 );
+
 
 const UserModel = mongoose.models.User || mongoose.model("User", userSchema);
 
