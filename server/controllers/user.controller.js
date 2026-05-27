e2430c73181a97a3178dc71ac18cdd9a2f643af6
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-27 19:27:44 +0530

    register pai is done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
-import UserModel from "../models/User.model.js"; 
+import UserModel from "../models/User.model.js";
 import { v2 as cloudinary } from "cloudinary";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -48,6 +49,19 @@ export const register = async (req, res) => {
 
     await newUser.save();
 
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    const isProduction = process.env.NODE_ENV === "production"; // development in server
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: isProduction, // boolean
+      sameSite: isProduction ? "none" : "lax", // correct logic
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
     return res.status(201).json({
       success: true,
       message: "User Registered",
@@ -56,7 +70,6 @@ export const register = async (req, res) => {
         image: newUser.image,
       },
     });
-
   } catch (error) {
     return res.status(500).json({
       success: false,
