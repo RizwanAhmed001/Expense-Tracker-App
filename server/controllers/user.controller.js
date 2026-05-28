986aab41741e2d91f72c4690864a0bda421d53f6
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-28 18:42:22 +0530

    login api is done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -77,3 +77,64 @@ export const register = async (req, res) => {
     });
   }
 };
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All Fields Are Mandatory!",
+      });
+    }
+
+    const isExist = await UserModel.findOne({ email });
+
+    if (!isExist) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials!",
+      });
+    }
+
+    const isCompared = await bcrypt.compare(password, isExist.password);
+
+    if (!isCompared) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials!",
+      });
+    }
+
+    const token = jwt.sign(
+      { id: isExist._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "7d" }
+    );
+
+    const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction ? "none" : "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User Login!",
+      user: {
+        name: isExist.name,
+        image: isExist.image,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
