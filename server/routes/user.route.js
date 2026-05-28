cc955cc67e8a625259c220b45b003aa3c477c300
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-28 18:47:27 +0530

    login route is done

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { register } from "../controllers/user.controller.js";
+import { login, register } from "../controllers/user.controller.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register", upload.single("image"), register)
+userRoute.post("/login", login)
 
 export default userRoute;
