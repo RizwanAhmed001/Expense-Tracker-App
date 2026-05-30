8a51d69828294da7dde1227d629a6236baef5a5e
Rizwan Ahmed <rizwanahmedrls96@gmail.com>
2026-05-30 17:52:02 +0530

    logout route is added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { login, register } from "../controllers/user.controller.js";
+import { login, logout, register } from "../controllers/user.controller.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register", upload.single("image"), register)
-userRoute.post("/login", login)
+userRoute.post("/login", login);
+userRoute.post("/logout", logout)
 
 export default userRoute;
